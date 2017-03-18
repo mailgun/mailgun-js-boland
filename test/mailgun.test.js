e9f270b2b15c69e7cdf516289f088a261097f588
Mats Högberg <mats@hgbrg.se>
2017-03-18 15:11:24 +0100

    Fixed failing tests for lists().members().add()

--- test/mailgun.test.js
+++ test/mailgun.test.js
@@ -902,7 +902,7 @@ module.exports = {
     });
   },
 
-  'test lists().members().add() without subscribed': function (done) {
+  'test lists().members().add() without upsert': function (done) {
     var members = [
       {
         address: 'Alice <alice@example.com>',
@@ -917,7 +917,7 @@ module.exports = {
 
     mailgun.lists(fixture.mailingList.address).members().add({members: members}, function (err, body) {
       assert.ok(err);
-      assert(/Missing parameter 'subscribed'/.test(err.message));
+      assert(/Missing parameter 'upsert'/.test(err.message));
       done();
     });
   },
@@ -937,7 +937,7 @@ module.exports = {
 
     mailgun.lists(fixture.mailingList.address).members().add({
       members: members,
-      subscribed: true
+      upsert: true
     }, function (err, body) {
       assert.ifError(err);
       assert.ok(body.list);
