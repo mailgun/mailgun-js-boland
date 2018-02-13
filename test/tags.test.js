1ce92b07959be814b0857153b6a7f176d18fc355
sy <shiyung@gmail.com>
2018-02-13 17:53:00 +0800

    1. fix standard style lint error. 2. Support tags statistics data. 3. test case.

--- test/tags.test.js
+++ test/tags.test.js
@@ -20,9 +20,46 @@ describe('Tags', () => {
   })
 
   it('test mailgun.tags().info()', (done) => {
-    mailgun.tags('tag1').info((err, body) => {
+    mailgun.tags('tag2').info((err, body) => {
       assert.ifError(err)
       assert.ok(body)
+      done()
+    })
+  })
+
+  it('test mailgun.tags().stats().info()', (done) => {
+    mailgun.tags('tag2').stats().info({
+      event: ['accepted', 'delivered', 'opened', 'clicked']
+    }, (err, body) => {
+      assert.ifError(err)
+      assert.ok(body)
+      done()
+    })
+  })
+
+  it('test mailgun.tags().aggregates().countries().list()', (done) => {
+    mailgun.tags('tag2').stats().aggregates().countries().list((err, body) => {
+      assert.ifError(err)
+      assert.ok(body)
+      assert.ok(body.countries)
+      done()
+    })
+  })
+
+  it('test mailgun.tags().aggregates().providers().list()', (done) => {
+    mailgun.tags('tag2').stats().aggregates().providers().list((err, body) => {
+      assert.ifError(err)
+      assert.ok(body)
+      assert.ok(body.providers)
+      done()
+    })
+  })
+
+  it('test mailgun.tags().aggregates().devices().list()', (done) => {
+    mailgun.tags('tag2').stats().aggregates().devices().list((err, body) => {
+      assert.ifError(err)
+      assert.ok(body)
+      assert.ok(body.devices)
       done()
     })
   })
