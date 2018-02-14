6b0ad8bcaae01a449afe3aa19e8dbf008ac1725b
Shih-Yung Lee <shiyung@gmail.com>
2018-02-14 09:11:58 +0800

    Support /tags/<tag>/stats (#193)
    * 1. fix standard style lint error.
    2. Support tags statistics data.
    3. test case.
    
    * rename test tag name back to tag1

--- test/tags.test.js
+++ test/tags.test.js
@@ -27,6 +27,43 @@ describe('Tags', () => {
     })
   })
 
+  it('test mailgun.tags().stats().info()', (done) => {
+    mailgun.tags('tag1').stats().info({
+      event: ['accepted', 'delivered', 'opened', 'clicked']
+    }, (err, body) => {
+      assert.ifError(err)
+      assert.ok(body)
+      done()
+    })
+  })
+
+  it('test mailgun.tags().aggregates().countries().list()', (done) => {
+    mailgun.tags('tag1').stats().aggregates().countries().list((err, body) => {
+      assert.ifError(err)
+      assert.ok(body)
+      assert.ok(body.countries)
+      done()
+    })
+  })
+
+  it('test mailgun.tags().aggregates().providers().list()', (done) => {
+    mailgun.tags('tag1').stats().aggregates().providers().list((err, body) => {
+      assert.ifError(err)
+      assert.ok(body)
+      assert.ok(body.providers)
+      done()
+    })
+  })
+
+  it('test mailgun.tags().aggregates().devices().list()', (done) => {
+    mailgun.tags('tag1').stats().aggregates().devices().list((err, body) => {
+      assert.ifError(err)
+      assert.ok(body)
+      assert.ok(body.devices)
+      done()
+    })
+  })
+
   it('test mailgun.tags().delete()', (done) => {
     mailgun.tags('tag1').delete((err, body) => {
       assert.ifError(err)
