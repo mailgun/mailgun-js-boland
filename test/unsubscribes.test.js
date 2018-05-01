f2db0e45d10f964bf2753670eb56d1d64028d8db
Bojan Djurkovic <dbojan@gmail.com>
2018-04-30 21:37:29 -0300

    Add special case to support multiple unsubscribes. Fixes #203

--- test/unsubscribes.test.js
+++ test/unsubscribes.test.js
@@ -12,48 +12,16 @@ describe('Unsubscribes', () => {
     setTimeout(done, 500)
   })
 
-  it('test unsubscribes().create() missing address', (done) => {
-    mailgun.unsubscribes().create({}, (err) => {
-      assert.ok(err)
-      assert(/Missing parameter 'address'/.test(err.message))
-      done()
-    })
-  })
-
-  it('test unsubscribes().create() missing tag', (done) => {
-    mailgun.unsubscribes().create({
-      'address': fixture.unsubscribe.address
-    }, (err) => {
-      assert.ok(err)
-      assert(/Missing parameter 'tag'/.test(err.message))
-      done()
-    })
-  })
-
-  it('test unsubscribes().create() invalid address type', (done) => {
-    mailgun.unsubscribes().create({
-      'address': 123,
-      'tag': fixture.unsubscribe.tag
-    }, (err) => {
-      assert.ok(err)
-      assert(/Invalid parameter type./.test(err.message))
-      done()
-    })
-  })
-
-  it('test unsubscribes().create() invalid tag type', (done) => {
-    mailgun.unsubscribes().create({
-      'address': fixture.unsubscribe.address,
-      'tag': 10
-    }, (err) => {
-      assert.ok(err)
-      assert(/Invalid parameter type./.test(err.message))
-      done()
-    })
-  })
-
   it('test unsubscribes().create()', (done) => {
     mailgun.unsubscribes().create(fixture.unsubscribe, (err, body) => {
+      assert.ifError(err)
+      assert.ok(body)
+      done()
+    })
+  })
+
+  it('test unsubscribes().create() multi', (done) => {
+    mailgun.unsubscribes().create([fixture.unsubscribe, fixture.unsubscribe2], (err, body) => {
       assert.ifError(err)
       assert.ok(body)
       done()
